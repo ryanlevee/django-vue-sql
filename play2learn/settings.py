d0a619c3df0801560954eae048c6c4c00ac8b3d8
karate-man <ryanlevee@gmail.com>
2022-10-23 11:55:48 -0700

    test

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'django-jokes-app-ryan' # REPLACE WITH BUCKET NAME
+AWS_STORAGE_BUCKET_NAME = 'django-vue-sql-aws' # REPLACE WITH BUCKET NAME
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_DEFAULT_ACL = None # Use S3 bucket's setting
