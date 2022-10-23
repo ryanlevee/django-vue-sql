3f85f92e3e0a0f616d97c77cfb1de588360078ca
karate-man <ryanlevee@gmail.com>
2022-10-23 14:54:41 -0700

    test

--- common/utils/email.py
+++ common/utils/email.py
@@ -4,7 +4,8 @@ from sendgrid.helpers.mail import Mail
 from django.conf import settings
 
 
-def send_email(to, subject, content, sender='ryanlevee@gmail.com'):
+# def send_email(to, subject, content, sender='ryanlevee@gmail.com'):
+def send_email(subject, content, sender='ryanlevee@gmail.com'):
     # sg = sendgrid.SendGridAPIClient(settings.SENDGRID_API_KEY) # this one came from old folder, presumably not working
     sg = sendgrid.SendGridAPIClient(api_key=settings.SENDGRID_API_KEY)
     mail = Mail(
