ebe71b3d2f30969a37b19b1d08f88a1c0f6b14b0
karate-man <ryanlevee@gmail.com>
2022-10-31 19:58:42 -0700

    cleanup

--- static/dist/js/app.js
+++ static/dist/js/app.js
@@ -868,7 +868,7 @@ module.exports = __webpack_require__.p + "img/lettercloud_wd_cmp.dc2afeed.png";
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cf5d3db2043ed0bb"; }
+/******/ 		__webpack_require__.h = function() { return "0b9a20970948bd29"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
